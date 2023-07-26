e09164d74dd4cedaf10cf56480311615e3e28ffb
Mateusz Iw <mateusz.daniel.iwanicki@gmail.com>
2023-07-26 15:39:45 +0200

    Fix bug - initialize List<Cell> in the Snake class

--- SnakeGameBlazor/Data/Snake.cs
+++ SnakeGameBlazor/Data/Snake.cs
@@ -4,5 +4,9 @@
     {
         public List<Cell> Cells { get; set; }
         public int Length { get; set; }
+        public Snake()
+        {
+            Cells = new List<Cell>();
+        }
     }
 }
