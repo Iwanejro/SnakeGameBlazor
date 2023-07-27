a334764fc22099fe310e12c09e55c55e479b8cbb
Mateusz Iw <mateusz.daniel.iwanicki@gmail.com>
2023-07-27 14:39:08 +0200

    Snake.cs - add Head and Tail properties
    SnakeGame.razor - refactor the page to use new properties

--- SnakeGameBlazor/Data/Snake.cs
+++ SnakeGameBlazor/Data/Snake.cs
@@ -4,9 +4,14 @@
     {
         public List<Cell> Cells { get; set; }
         public int Length { get; set; }
+        public Cell Head { get; set; }
+        public Cell Tail { get; set; }
+
         public Snake()
         {
             Cells = new List<Cell>();
+            Head = new Cell();
+            Tail = new Cell();
         }
     }
 }
