8e6c1f3054f7794c1706ef696241b16c09bf0b9e
Mateusz Iw <mateusz.daniel.iwanicki@gmail.com>
2023-08-01 14:50:27 +0200

    Add feature: On winning condition snake goes into a hole and the "congrats!" inscription is displayed.

--- SnakeGameBlazor/Constants/GridColors.cs
+++ SnakeGameBlazor/Constants/GridColors.cs
@@ -2,16 +2,19 @@
 {
     public static class GridColors
     {
-        public const string Black = "black";
-        public const string Blue = "blue";
-        public const string Green = "green";
-        public const string Red = "red";
-        public const string Beer = "beer";
-        public const string Kebab = "kebab";
+        public const string Black = "black.png";
+        public const string Blue = "blue.png";
+        public const string Green = "green.png";
+        public const string Red = "red.png";
+        public const string Beer = "beer.png";
+        public const string Kebab = "kebab.png";
+        public const string Yellow = "yellow.png";
+        public const string Hole = "hole.png";
+        
 
-        public const string EyesUp = "eyes_up";
-        public const string EyesDown = "eyes_down";
-        public const string EyesLeft = "eyes_left";
-        public const string EyesRight = "eyes_right";
+        public const string EyesUp = "eyes_up.png";
+        public const string EyesDown = "eyes_down.png";
+        public const string EyesLeft = "eyes_left.png";
+        public const string EyesRight = "eyes_right.png";
     }
 }
---
+++ SnakeGameBlazor/Constants/Letters.cs
@@ -0,0 +1,15 @@
+﻿namespace SnakeGameBlazor.Constants
+{
+    public static class Letters
+    {
+        public const string A = "A.png";
+        public const string C = "C.png";
+        public const string ExclamationMark = "ExclamationMark.png";
+        public const string G = "G.png";
+        public const string N = "N.png";
+        public const string O = "O.png";
+        public const string R = "R.png";
+        public const string S = "S.png";
+        public const string T = "T.png";
+    }
+}
